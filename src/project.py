5b15d0869aebea8d050d8f5f148fd185ab9d3976
Rachel Santos <rms240001@utdallas.edu>
2025-12-06 17:02:58 -0600

    adding 'press enter' so that user can choose canvas size after into welcome screen

--- src/project.py
+++ src/project.py
@@ -17,14 +17,22 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                if event.key == pygame.K_RETURN:
+                    running = False
 
         # --- Drawing code ---
         screen.fill((255, 255, 255))
 
-        font = pygame.font.SysFont(None, 48)
+        font = pygame.font.SysFont(None, 72)
         text_surface = font.render("Welcome to Pixel Art Editor!", True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=screen.get_rect().center)
         screen.blit(text_surface, text_rect)
+
+        small_font = pygame.font.SysFont(None, 36)
+        prompt_surface = small_font.render("Press ENTER to continue...", True, (0, 0, 0))
+        prompt_rect = prompt_surface.get_rect(center=(resolution[0] // 2, resolution[1] // 2 + 100))
+
+        screen.blit(prompt_surface, prompt_rect)
 
         pygame.display.flip()
 
