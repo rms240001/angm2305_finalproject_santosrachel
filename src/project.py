680584846a53f4259c9ffc21aa13e26ad1ed112a
Rachel Santos <rms240001@utdallas.edu>
2025-12-06 15:37:14 -0600

    setup initial repo structure; planning for first feature add

---
+++ src/project.py
@@ -0,0 +1,14 @@
+import pygame
+
+def main():
+    """
+    Initializes pygame, creates window, and runs the main game loop.
+    """
+    # TODO: Initialize game, display user with simple UI to interact with
+
+    # Ask user the canvas size they want to use, accepts user input
+    
+    # Create canvas size based on user selected option
+
+if __name__ == "__main__":
+    main()
